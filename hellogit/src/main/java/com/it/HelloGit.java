2054984e49a8be2cb42db233b52bfc48209d63d2
code19971001 <p15194420545@163.com>
2020-10-28 15:05:23 +0800

    这是我的第一个git项目

---
+++ hellogit/src/main/java/com/it/HelloGit.java
@@ -0,0 +1,11 @@
+package com.it;
+
+/**
+ * @author : code1997
+ * @date :2020-10-2020/10/28 14:44
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
